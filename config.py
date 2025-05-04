f126b1ac178001f459e028e08fd2db05791bf2d4
armanmd85000 <154997050+armanmd85000@users.noreply.github.com>
2025-05-04 15:09:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ class Config():
     API_ID = ""
     API_HASH = ""
     PHONE_NUMBER = "+91987654321"
-    NAME = "GreyTime"
+    NAME = "Lightskillhub"
     SRC_CHAT_ID = -1001234567890
     DEST_CHAT_ID = -1001234567891
